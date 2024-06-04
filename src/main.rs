df8050c4a70e5f6bf863fcf012f180f07fc63bbb
Mark Jensen <marknjensen@outlook.com>
2024-06-04 22:28:30 +0200

    works with adb

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod database;
 mod services;
 
 use axum::{http::StatusCode, routing::get};
-use std::net::SocketAddr;
+use std::net::{Ipv4Addr, SocketAddr};
 
 #[tokio::main]
 async fn main() {
@@ -12,12 +12,13 @@ async fn main() {
         .nest("/api/v1", controllers::api::get_v1_api().await)
         .fallback((StatusCode::NOT_FOUND, "Not Found"));
 
-    let addr = SocketAddr::from(([0, 0, 0, 0, 0, 0, 0, 0], 3000));
+    // Cannot make IPv6 work because it infefers with android dual stack :(
+    let addr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, 3000));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
-    println!("Listening on {}", addr);
+    println!("Listening on http://{}", addr);
     axum::serve(listener, app).await.unwrap();
 }
 
