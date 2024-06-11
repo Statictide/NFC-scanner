3a8d665f7897717a088af8600617ed9c71140bbb
Mark Jensen <marknjensen@outlook.com>
2024-06-12 00:33:14 +0200

    auth

--- src/controllers/auth_routes.rs
+++ src/controllers/auth_routes.rs
@@ -1,50 +1,31 @@
 use axum::{
     http::StatusCode,
-    response::{IntoResponse, Response},
+    response::IntoResponse,
     routing::post,
     Json, Router,
 };
 
 use crate::services::auth_service;
 
+use super::errors::AppResult;
+
 pub fn get_auth_routes() -> Router {
     Router::new().route("/", post(authenticate))
 }
 
-async fn authenticate(Json(auth_user): axum::extract::Json<AuthDTO>) -> Response {
+async fn authenticate(Json(auth_user): axum::extract::Json<AuthDTO>) ->  AppResult<impl IntoResponse> {
     let session_result = auth_service::authenticate(auth_user.username).await;
-    tracing::error!("Test");
 
     let Ok(session) = session_result else {
         tracing::error!("Failed to authenticate: {:?}", session_result);
-        return (StatusCode::UNAUTHORIZED, "Unauthorized").into_response();
+        return Ok((StatusCode::UNAUTHORIZED, "Unauthorized").into_response());
     };
 
     let auth_response_dto = TokenResponse {
         token: session.token,
     };
 
-    return (StatusCode::CREATED, Json(auth_response_dto)).into_response();
-}
-
-async fn _login_handler(// TypedHeader(Authorization(auth)): TypedHeader<Authorization<Basic>>,
-) -> impl IntoResponse {
-    // Extract the username and password
-    let username = "auth.username()";
-    let password = "auth.password()";
-
-    // Validate credentials (this is just an example, replace with your own logic)
-    if username == "user" && password == "password" {
-        // Generate a token (for simplicity, we'll use the username)
-        let token = username;
-        let response = TokenResponse {
-            token: token.to_string(),
-        };
-
-        (StatusCode::OK, Json(response)).into_response()
-    } else {
-        (StatusCode::UNAUTHORIZED, Json("Invalid credentials")).into_response()
-    }
+    return Ok((StatusCode::CREATED, Json(auth_response_dto)).into_response());
 }
 
 #[derive(serde::Deserialize)]
