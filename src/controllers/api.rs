9b42110502728a46cb7ce5bd4a8ad7b266cb4c12
Mark Jensen <marknjensen@outlook.com>
2024-09-04 22:05:49 +0200

    cleanup

--- src/controllers/api.rs
+++ src/controllers/api.rs
@@ -22,33 +22,53 @@ mod update {
 
     static SEMVER_REGEX: OnceCell<regex::Regex> = OnceCell::const_new();
     pub async fn check_for_update(Json(body): axum::extract::Json<CheckForUpdateDTO>) -> AppResult<impl IntoResponse> {
-        let regex = SEMVER_REGEX
-            .get_or_init(|| async { Regex::new("^(?<major>[0-9]+)\\.(?<minor>[0-9]+)\\.(?<patch>[0-9]+)$").unwrap() })
-            .await;
-        let cap = regex
-            .captures(&body.version)
-            .ok_or(AppError::BadRequest("Version is not a valid semver string".to_string()))?;
-        let major = cap["major"].parse::<u8>().unwrap();
-        let minor = cap["minor"].parse::<u8>().unwrap();
-        let patch = cap["patch"].parse::<u8>().unwrap();
-        // let alpha = cap["alpha"];
+        let (major, minor, patch) = parse_semver(body).await?;
 
         // v0.0.0
+        #[allow(unused_comparisons)]
         if major >= 0 && minor >= 0 && patch >= 0 {
             let response = CheckForUpdateResponseDTO {
                 update_required: false,
+                update_recommended: false,
                 message: "All good".to_string(),
             };
             return Ok((StatusCode::OK, Json(response)));
         }
 
         let response = CheckForUpdateResponseDTO {
-            update_required: true,
+            update_required: false,
+            update_recommended: true,
             message: "Please update".to_string(),
         };
         Ok((StatusCode::OK, Json(response)))
     }
 
+    async fn parse_semver(body: CheckForUpdateDTO) -> AppResult<(u8, u8, u8)> {
+        let regex = SEMVER_REGEX
+            .get_or_init(|| async { Regex::new("^(?<major>[0-9]+)\\.(?<minor>[0-9]+)\\.(?<patch>[0-9]+)(?:-([0-9A-Za-z-]+(?:\\.[0-9A-Za-z-]+)*))?(?:\\+[0-9A-Za-z-]+)?$").unwrap() })
+            .await;
+        let cap = regex
+            .captures(&body.version)
+            .ok_or(AppError::BadRequest("Version is not a valid semver string".to_string()))?;
+        let major = cap.name("major")
+            .ok_or(AppError::BadRequest("".to_string()))?
+            .as_str()
+            .parse::<u8>()
+            .map_err(|_| AppError::BadRequest("Major is not a valid number".to_string()))?;
+        let minor = cap.name("minor")
+            .ok_or(AppError::BadRequest("".to_string()))?
+            .as_str()
+            .parse::<u8>()
+            .map_err(|_| AppError::BadRequest("Minor is not a valid number".to_string()))?;
+        let patch = cap.name("patch")
+            .ok_or(AppError::BadRequest("".to_string()))?
+            .as_str()
+            .parse::<u8>()
+            .map_err(|_| AppError::BadRequest("Patch is not a valid number".to_string()))?;
+
+        Ok((major, minor, patch))
+    }
+    
     #[allow(dead_code)]
     #[derive(serde::Deserialize)]
     pub struct CheckForUpdateDTO {
@@ -65,6 +85,7 @@ mod update {
     #[derive(serde::Serialize)]
     pub struct CheckForUpdateResponseDTO {
         pub update_required: bool,
+        pub update_recommended: bool,
         pub message: String,
     }
 }
